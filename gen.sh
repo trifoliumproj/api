999ea44472e6fd483cf84e60a18015b6cfc19bda
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-20 14:56:56 +0700

    gen.sh: update

--- gen.sh
+++ gen.sh
@@ -4,4 +4,9 @@ docker pull namely/protoc-all
 docker run -v `pwd`:/defs namely/protoc-all \
 	-f nhentai.proto \
 	-l go \
-	-o generated
+	-o generated/nhentai
+docker run -v `pwd`:/defs namely/protoc-all \
+	-f nhentai.proto \
+	-l node \
+	-o generated/nhentai \
+	--with-typescript
