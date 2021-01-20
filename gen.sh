353b1b199265f1a7a1d342662c5f935936dfed07
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-20 19:06:34 +0700

    feat(gen.sh): add support for hentai-cafe.proto

--- gen.sh
+++ gen.sh
@@ -5,8 +5,20 @@ docker run -v `pwd`:/defs namely/protoc-all \
 	-f nhentai.proto \
 	-l go \
 	-o generated/nhentai
+
 docker run -v `pwd`:/defs namely/protoc-all \
 	-f nhentai.proto \
 	-l node \
 	-o generated/nhentai \
+	--with-typescript
+
+docker run -v `pwd`:/defs namely/protoc-all \
+	-f hentai-cafe.proto \
+	-l node \
+	-o generated/hentai-cafe \
+
+docker run -v `pwd`:/defs namely/protoc-all \
+	-f hentai-cafe.proto \
+	-l node \
+	-o generated/hentai-cafe \
 	--with-typescript
